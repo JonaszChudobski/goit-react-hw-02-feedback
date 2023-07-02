4cffe704756b7f5bd56810ba2be270b9e1f7a117
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-07-02 13:08:48 +0200

    task four done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,56 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleAdding = e => {
+    if (e.currentTarget.id === 'good') {
+      setGood(good + 1);
+    } else if (e.currentTarget.id === 'neutral') {
+      setNeutral(neutral + 1);
+    } else {
+      setBad(bad + 1);
+    }
   };
 
-  handleAdding = e => {
-    const { id } = e.target;
-    this.setState(prevState => ({ [id]: prevState[id] + 1 }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = (good, neutral, bad) => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return Math.round((good / this.countTotalFeedback()) * 100) || 0;
+  const countPositiveFeedbackPercentage = (good, neutral, bad) => {
+    return (
+      Math.round((good / countTotalFeedback(good, neutral, bad)) * 100) || 0
+    );
   };
 
-  render() {
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.state}
-            onLeaveFeedback={this.handleAdding}
-          ></FeedbackOptions>
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() !== 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            ></Statistics>
-          ) : (
-            <Notification />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions onLeaveFeedback={handleAdding}></FeedbackOptions>
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback(good, neutral, bad) !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback(good, neutral, bad)}
+            positivePercentage={countPositiveFeedbackPercentage(
+              good,
+              neutral,
+              bad
+            )}
+          ></Statistics>
+        ) : (
+          <Notification />
+        )}
+      </Section>
+    </>
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,28 +1,22 @@
 import css from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+export const FeedbackOptions = ({ onLeaveFeedback }) => {
   return (
     <div className={css.feedback}>
-      {Object.keys(options).map(option => (
-        <button
-          key={option}
-          id={option}
-          type="button"
-          onClick={onLeaveFeedback}
-        >
-          {option}
-        </button>
-      ))}
+      <button key="good" id="good" type="button" onClick={onLeaveFeedback}>
+        Good
+      </button>
+      <button key="neutral" id="neutral" type="button" onClick={onLeaveFeedback}>
+        Neutral
+      </button>
+      <button key="bad" id="bad" type="button" onClick={onLeaveFeedback}>
+        Bad
+      </button>
     </div>
   );
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.shape({
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-  }),
   onLeaveFeedback: PropTypes.func,
 };
